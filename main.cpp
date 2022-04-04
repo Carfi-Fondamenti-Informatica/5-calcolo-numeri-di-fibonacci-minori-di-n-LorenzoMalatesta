cec60f0a2b316986c58d0accbb240ff44690eb75
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-04-04 20:34:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
     int n=0, num_0=0, num_1=1, num_2;
     cin >> n;
+    if (n==1) {
+        cout << "1" <<endl;
+    }
     for (num_2=0; num_2<=n; num_2++) {
         num_2=num_0+num_1;
         if (num_2<=n) {
